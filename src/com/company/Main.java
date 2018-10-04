61a2d8e0947b92e431957dc36b69d6796b52785d
yodevhail <yodevhail@gmail.com>
2018-10-04 09:45:57 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+// Assigning my name into a variable and print it out to console
+    public static void main(String[] args) {
+        String myname = "yonas";
+        System.out.println(myname);
+	// write your code here
+    }
+}
